f7134519455e7776992e06b156511438ff6f908b
nethore <nikolas.ethore@gmail.com>
2016-11-18 16:38:22 +0100

    Push Original

---
+++ js/users.js
@@ -0,0 +1,215 @@
+(function(){
+
+  "use strict";
+
+  angular.module('appUsers', ['rzModule'])
+        .controller('UsersCtrl', UsersCtrl)
+        .filter('onlyMajeur', function(){
+          return function(input, bool){
+            var out = [];
+            if(bool) {
+              angular.forEach(input, function(user){
+                if(user.age >= 18){
+                  out.push(user);
+                }
+              });
+            } else { out = input; }
+            return out;
+          };
+        })
+        .filter('whatCity', function(){
+          return function(input, city){
+            var out = [];
+            if(city != "all") {
+              angular.forEach(input, function(user){
+                if(user.ville == city){
+                  out.push(user);
+                }
+              });
+            } else { out = input; }
+            return out;
+          };
+        });
+
+  function UsersCtrl($scope, $filter){
+
+    console.log("Scope Chargée");
+
+    $scope.citySelected = "all";
+    $scope.nbrAffiches = 15;
+    $scope.users = [
+      {
+        nom: 'Ethore',
+        prenom: 'Nikolas',
+        age: 24,
+        ville: 'Lyon',
+        sexe: true,
+        img: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/090/2cb/07256b2.jpg',
+        coord: {
+          lat: 45.954356,
+          long: 4.694858
+        }
+      },
+      {
+        nom: 'Beaufils',
+        prenom: 'Lucas',
+        age: 24,
+        ville: 'Paris',
+        sexe: true,
+        img: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/090/2cb/07256b2.jpg',
+        coord: {
+          lat: 45.954356,
+          long: 4.694858
+        }
+      },
+      {
+        nom: 'Ethore',
+        prenom: 'Nicole',
+        age: 74,
+        ville: 'Pommiers',
+        sexe: false,
+        img: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/090/2cb/07256b2.jpg',
+        coord: {
+          lat: 45.954356,
+          long: 4.694858
+        }
+      },
+      {
+        nom: 'Ethore',
+        prenom: 'Fabien',
+        age: 37,
+        ville: 'Lyon',
+        sexe: true,
+        img: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/090/2cb/07256b2.jpg',
+        coord: {
+          lat: 45.954356,
+          long: 4.694858
+        }
+      },
+      {
+        nom: 'Biber',
+        prenom: 'Justine',
+        age: 15,
+        ville: 'Paris',
+        sexe: false,
+        img: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/090/2cb/07256b2.jpg',
+        coord: {
+          lat: 45.954356,
+          long: 4.694858
+        }
+      },
+      {
+        nom: 'David',
+        prenom: 'John',
+        age: 17,
+        ville: 'Los Angeles',
+        sexe: true,
+        img: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/090/2cb/07256b2.jpg',
+        coord: {
+          lat: 45.954356,
+          long: 4.694858
+        }
+      },
+      {
+        nom: 'Clavier',
+        prenom: 'Christian',
+        age: 65,
+        ville: 'Marseille',
+        sexe: true,
+        img: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/090/2cb/07256b2.jpg',
+        coord: {
+          lat: 45.954356,
+          long: 4.694858
+        }
+      },
+      {
+        nom: 'Spears',
+        prenom: 'Britney',
+        age: 35,
+        ville: 'Los Angeles',
+        sexe: false,
+        img: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/090/2cb/07256b2.jpg',
+        coord: {
+          lat: 45.954356,
+          long: 4.694858
+        }
+      },
+      {
+        nom: 'La Fripouille',
+        prenom: 'Jacquouille',
+        age: 33,
+        ville: 'Paris',
+        sexe: true,
+        img: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/090/2cb/07256b2.jpg',
+        coord: {
+          lat: 45.954356,
+          long: 4.694858
+        }
+      },
+      {
+        nom: 'Beaufils',
+        prenom: 'Maureen',
+        age: 27,
+        ville: 'Marseille',
+        sexe: false,
+        img: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/090/2cb/07256b2.jpg',
+        coord: {
+          lat: 45.954356,
+          long: 4.694858
+        }
+      }
+    ];
+
+    $scope.ajouterUser = function(){
+
+      console.log("Formulaire envoyé");
+      $scope.users.push({
+        nom: $scope.nom,
+        prenom: $scope.prenom,
+        age: parseInt($scope.age),
+        ville: $scope.ville,
+        sexe: $scope.sexe,
+        img: $scope.img
+
+      });
+
+      $scope.nom = "";
+      $scope.prenom = "";
+      $scope.age = "";
+      $scope.ville = "";
+      $scope.sexe = "";
+      $scope.img = "";
+
+
+    };
+
+    $scope.supprUser = function(id){
+      $scope.users.splice(id, 1);
+    };
+
+    $scope.verifImg = function(){
+      var regex = /[\S]*(\.gif)$/i;
+      if(regex.test($scope.img)) {
+        $scope.classImgForm = "has-success";
+      }
+      else {
+        $scope.classImgForm = "has-error";
+      }
+    };
+
+    $scope.triParAge = function() {
+      console.log($scope.triAge);
+      $scope.varTri = "age";
+      if ($scope.triAge == "down") {
+        $scope.reverseTri = false;
+      }
+      else if ($scope.triAge == "up"){
+        $scope.reverseTri = true;
+      }
+      console.log($scope.users);
+
+    };
+
+  }
+
+}());
---
+++ readme.md
@@ -0,0 +1,50 @@
+
+/**
+* Exercice 2: Avec Boostrapp Twitter et la DOC JS ;)
+*/
+
+
+/*
+*
+* + Créer un bouton par utilisateur pour pouvoir supprimer
+* un utilisateur
+*
+* + Afficher le sexe par utilisateur: Homme ou Femme
+*
+* + Ajouter un Avatar (url d'image) par utilisateur lors de l'affichage des utilisateurs
+et de la création d'utilisateur (ng-src). Valider par un blur que la photo soit gif
+*
+* + Créer une liste déroulante permetant de trier par age avec 2 options:
+    - Le plus Jeune
+    - Le plus Vieux
+
+   AU changement d'item selectionné, cela me trie le tableau du plus jeune au plus vieux OU
+   		du plus vieux au plus jeune
+
+  + Ajouter une checkbox pour voir apparaitre que les utilisateurs majeur quand elle est cochés
+
+  + Ajouter 3 bouttons radios Paris - Lyon  - Marseille pour filtrer les utilisateurs par ville
+  et un boutton "Toutes les villes" pour voir tout les utilisateurs
+
+  + Ajouter par utilisateur un tableaux de Sports (au choix: Foot, Basket Baseball,Rugby, Handball )
+    PuisCréer
+
+
+  + Ajouter 10 utilisateurs dans votre liste d'utilisateurs puis
+  	créer un input range (ou range slider en jquery/angular)
+    pour limiter le nombre d'utilisateurs
+
+  + Ajouter les coordonées longitude et latitude dans un objet "coords" PAR utilisateurs
+    et placer les utilisateurs sur une Google Map tuto: https://openclassrooms.com/courses/google-maps-javascript-api-v3
+    ou module en AngularJS
+
+
+    Bonus: Décliner le meme exercice mais en chargeant l'API de JsonPlaceholder
+    sous l'url: https://jsonplaceholder.typicode.com/users
+    Pour cela, voir comment utliser l'objet $http
+
+
+
+  NB: ****Breaking News**** VOUS POUVEZ UTILISER LA LIBRAIRIES JS "LODASH" pour toutes vos traitement de tableaux , objets...
+
+  
